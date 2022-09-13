284cf275f5864bf19c1c8ad449a72519014cd96f
Caio Vinicius <caio.vinnicius2k@gmail.com>
2022-09-13 13:54:56 -0300

    mobile app part 2 wip

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,33 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <Button title='Send 1'></Button>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+    </Background>
   );
 }
-
-interface ButtonProps {
-  title: string,
-}
-
-function Button(props: ButtonProps ) {
-  return (
-    <TouchableOpacity>
-      <Text> { props.title } </Text>
-    </TouchableOpacity>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
